a7f70dd3ae1576ed1ebaff70a89c7e82b6cfd2ad
Bradone911 <baholland1@buffs.wtamu.edu>
2019-02-05 20:43:48 -0600

    IT WORKS Yayyy

--- Models/MvcMovieContext.cs
+++ Models/MvcMovieContext.cs
@@ -13,6 +13,7 @@ namespace MvcMovie.Models
         {
         }
 
-        public DbSet<MvcMovie.Models.Movie> Movie { get; set; }
+        public DbSet<Movie> Movie { get; set; }
+        //public DbSet<MvcMovie.Models.Movie> Movie { get; set; }
     }
 }
